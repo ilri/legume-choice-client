88e5e277abfea355e8f43ba0ef9bad3b11fe12a6
l-gorman <lgorman@turing.ac.uk>
2021-02-17 16:50:36 +0000

    adding agro-eco filter

--- src/components/data-entry-component/agro-ecological-filter.js
+++ src/components/data-entry-component/agro-ecological-filter.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
+import { Table, Form } from "react-bootstrap";
+
+import "./agro-ecological-filter.css";
 
 class AgroEco extends Component {
     render() {
         return (
             <div>
                 <h2>Agroecological Filter</h2>
+
+                <div className="table-container">
+                    <Table
+                        striped
+                        bordered
+                        hover
+                        className="agro-eco-table-style"
+                    >
+                        <thead>
+                            <tr>
+                                <th>Biofilters</th>
+                                <th>Site Values</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Rainfall (mm/year)</td>
+                                <td>
+                                    <Form.Control type="number" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Temperature (mean °C/month) </td>
+                                <td>
+                                    <Form.Control type="number" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Altitude (average masl)</td>
+                                <td>
+                                    <Form.Control type="number" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Soil pH (average)</td>
+                                <td>
+                                    <Form.Control type="number" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         );
     }
