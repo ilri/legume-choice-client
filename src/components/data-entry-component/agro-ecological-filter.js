434edd2a5670117b033ac6c647b6ad5b3b1766cc
l-gorman <lgorman@turing.ac.uk>
2021-02-19 10:44:57 +0000

    finished agro-eco-filter

--- src/components/data-entry-component/agro-ecological-filter.js
+++ src/components/data-entry-component/agro-ecological-filter.js
@@ -6,6 +6,77 @@ import agroEcoData from "./agro-ecological-data";
 import "./agro-ecological-filter.css";
 
 class AgroEco extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = agroEcoData;
+    }
+
+    componentDidMount() {
+        console.log(this.state);
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+    tableHeader = () => {
+        return (
+            <thead>
+                <tr>
+                    <th>Biofilters</th>
+                    <th>Site Values</th>
+                </tr>
+            </thead>
+        );
+    };
+
+    tableBody = () => {
+        return (
+            <tbody>
+                {this.state.biofilters.map((biofilter) => {
+                    return (
+                        <tr>
+                            <td>{biofilter.name}</td>
+                            <td>
+                                <Form.Control
+                                    type="number"
+                                    defaultValue={biofilter.value}
+                                    onClick={(event) => {
+                                        this.handleChange(event, biofilter);
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        );
+    };
+
+    handleChange = (event, biofilter) => {
+        const bioFiltersArray = this.state.biofilters;
+
+        if (
+            event.target.value < biofilter.minValue ||
+            event.target.value > biofilter.maxValue
+        ) {
+            alert("Outside of range");
+            return;
+        }
+
+        bioFiltersArray.forEach((biofilterSubset, biofilterIndex) => {
+            if (biofilterSubset === biofilter) {
+                bioFiltersArray[biofilterIndex].value = parseInt(
+                    event.target.value
+                );
+            }
+        });
+
+        this.setState({
+            biofilters: bioFiltersArray,
+        });
+    };
+
     render() {
         return (
             <div>
@@ -18,38 +89,9 @@ class AgroEco extends Component {
                         hover
                         className="agro-eco-table-style"
                     >
-                        <thead>
-                            <tr>
-                                <th>Biofilters</th>
-                                <th>Site Values</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Rainfall (mm/year)</td>
-                                <td>
-                                    <Form.Control type="number" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Temperature (mean °C/month) </td>
-                                <td>
-                                    <Form.Control type="number" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Altitude (average masl)</td>
-                                <td>
-                                    <Form.Control type="number" />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Soil pH (average)</td>
-                                <td>
-                                    <Form.Control type="number" />
-                                </td>
-                            </tr>
-                        </tbody>
+                        <this.tableHeader />
+
+                        <this.tableBody />
                     </Table>
                 </div>
             </div>
