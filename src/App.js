09027d26d2fc1830990bf909a6d95bf225c64a33
l-gorman <lgorman@turing.ac.uk>
2021-02-22 12:34:16 +0000

    changing to hash router for github pages deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, createContext } from "react";
 
 import {
-    BrowserRouter as Router,
+    //BrowserRouter as Router,
+    HashRouter as Router,
     Switch,
     Route,
     Redirect,
